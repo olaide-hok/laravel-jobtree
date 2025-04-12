0b45ef2c6715095f0f90b2332bafa3fea8ab081f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-12 14:18:37 +0100

    add link tag to point to custom stylesheet
    use if directive to conditionally render hero component on homepage

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header/>
+    @if(request()->is('/'))
+        <x-hero/>
+    @endif
     <main class="container mx-auto p-4 mt-4">
        {{$slot}}
     </main>
