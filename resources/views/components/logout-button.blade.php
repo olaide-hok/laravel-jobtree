7b536db364772749e85bfcbe359d02b9379f4f4b
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-17 07:39:08 +0100

    add 'hover:cursor-pointer' utility class

--- resources/views/components/logout-button.blade.php
+++ resources/views/components/logout-button.blade.php
@@ -1,6 +1,6 @@
 <form method="POST" action="{{route('logout')}}">
     @csrf
-    <button type="submit" class="text-white">
+    <button type="submit" class="text-white hover:cursor-pointer">
         <i class="fa fa-sign-out"></i> Logout
     </button>
 </form>
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -3,7 +3,7 @@
         value="{{request('keywords')}}" />
     <input type="text" name="location" placeholder="Location" class="w-full md:w-72 px-4 py-3 bg-white focus:outline-none"
         value="{{request('location')}}" />
-    <button class="w-full md:w-auto bg-green-700 hover:bg-green-600 text-white px-4 py-3 focus:outline-none">
+    <button class="w-full md:w-auto bg-green-700 hover:bg-green-600 text-white px-4 py-3 focus:outline-none hover:cursor-pointer">
         <i class="fa fa-search mr-1"></i> Search
     </button>
 </form>
